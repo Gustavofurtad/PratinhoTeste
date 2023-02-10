69425054d34f5aa82b1f89e4e2defcd52a1a7be3
Gustavo Furtado <110691747+Gustavofurtad@users.noreply.github.com>
2023-02-10 11:30:46 -0300

    Add files via upload

---
+++ Js.js
@@ -0,0 +1,9 @@
+function Burgue(){
+    var Cabeca = window.document.querySelector('Header')
+    if(Cabeca.style.display == "block"){
+        Cabeca.style.display = "none"
+
+    }else{
+        Cabeca.style.display = "block"
+    }
+}
